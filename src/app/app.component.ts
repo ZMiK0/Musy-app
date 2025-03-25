d069f73a56b49af055e4eb56391ab7bcbef70bc0
ZMiK0 <kbelz21z@gmail.com>
2025-03-25 22:20:29 +0100

    MainScreen do

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { invoke } from "@tauri-apps/api/core";
 import { SidebarComponent } from "./items/sidebar/sidebar.component";
+import { MainScreenComponent } from "./items/mainscreen/mainscreen.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, SidebarComponent],
+  imports: [CommonModule, SidebarComponent, MainScreenComponent],
   templateUrl: './app.component.html',
   styleUrl: '../styles.css'
 })
