f49e76d18f85d23ca404313a522637dd5a31568d
ZMiK0 <kbelz21z@gmail.com>
2025-03-28 16:43:54 +0100

    Testing Audio

--- src/app/items/playbar/playbar.component.ts
+++ src/app/items/playbar/playbar.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { invoke } from "@tauri-apps/api/core";
+
 
 @Component({
   selector: 'app-playbar',
@@ -13,6 +14,12 @@ export class PlaybarComponent {
   progress = 0; 
   volume = 0;
 
+  isPlaying = false;
+
+
+  //!-PRUEBAS
+  song = new Audio("../../../assets/TornadoOfSouls.mp3"); //! Ya no funciona, en el futuro esto estará llevado por una base de datos y rust asi que np
+
   onInput(event: Event) {
     const input = event.target as HTMLInputElement;
     this.progress = (parseInt(input.value) / parseInt(input.max)) * 100;
@@ -22,4 +29,24 @@ export class PlaybarComponent {
     const inputVolume = event.target as HTMLInputElement;
     this.volume = (parseInt(inputVolume.value) / parseInt(inputVolume.max)) * 100
   }
+
+  async initAudio() {
+    this.song.load()
+  }
+
+  async onPlay() {
+
+    if (!this.song) await this.initAudio();
+
+    if (!this.isPlaying) {
+      this.song.play()
+      console.log("Cancion iniciada")
+    } else {
+      this.song.pause()
+      console.log("Cancion pausada")
+    }
+    this.isPlaying = !this.isPlaying;
+  }
+
+
 }
